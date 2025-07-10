1f5aeb66ce5ff4938f1fb61bfe236ee3ef53b0a1
Artur Tytomyr <artur.tytomyr@globallogic.com>
2025-07-10 23:42:36 +0300

    Remove dim_time

--- promova_test_task/models/dim_calendar.sql
+++ promova_test_task/models/dim_calendar.sql
@@ -1,11 +1,11 @@
-   {{
-     config(
-       materialized='table'
-     )
-   }}
+{{
+ config(
+   materialized='table'
+ )
+}}
 
-   {{ dbt_date.get_date_dimension(
-       start_date="2015-01-01",
-       end_date="2025-12-31"
-      )
-   }}
+{{ dbt_date.get_date_dimension(
+   start_date="2015-01-01",
+   end_date="2025-12-31"
+  )
+}}
