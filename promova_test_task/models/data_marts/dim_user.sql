2fe886d8483bbf3a8e070f70d26eb6498d20c934
Artur Tytomyr <artur.tytomyr@globallogic.com>
2025-07-12 19:29:21 +0300

    fix models for user and user_activity

--- promova_test_task/models/data_marts/dim_user.sql
+++ promova_test_task/models/data_marts/dim_user.sql
@@ -14,15 +14,13 @@ devices_per_user AS (
     COUNT(*) AS devices
   FROM {{ ref("dim_device") }}
   GROUP BY user_id
-),
-
+)
 
 SELECT
   GENERATE_UUID() AS user_sk,
   tr.user_id,
   COALESCE(s.subscriptions, 0) AS subscriptions,
-  COALESCE(d.devices, 0) AS devices,
-  coalesce(t.transactions_made) as transactions,
+  COALESCE(d.devices, 0) AS devices
 FROM {{ ref("transactions_raw") }} tr
 LEFT JOIN subscriptions_per_user s ON tr.user_id = s.user_id
 LEFT JOIN devices_per_user d ON tr.user_id = d.user_id
